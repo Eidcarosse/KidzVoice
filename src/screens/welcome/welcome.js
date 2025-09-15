10d069d3c2afe1016550483a38f9424c214dfae6
Ahmad Saffiullah <111109170+ahmadkhan402@users.noreply.github.com>
2025-09-15 22:42:32 +0500

    update

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import commonStyles from "../../utils/commonStyles";
+import { fonts } from "../../utils/fonts";
 
 export default function Welcome() {
   return (
     <View style={commonStyles.parentView}>
-      <Text>welcome</Text>
+      <Text style={{ fontFamily: fonts.regular }}>welcome</Text>
     </View>
   );
 }
