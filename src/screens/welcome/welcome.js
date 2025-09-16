450d1046037989ddd8e4b91932fe8245ec134bb1
Malik Haider <malikhaidera46@gmail.com>
2025-09-16 18:41:18 +0500

    style

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -8,21 +8,33 @@ import colors from "../../utils/AppColors";
 import StatusBarWrapper from "../../components/customStatusbar";
 import Images from "../../assets/images";
 import styles from "./styles";
+import { height } from "../../utils/Dimensions";
 
 export default function Welcome() {
   return (
     <StatusBarWrapper>
-      <View style={commonStyles.parentView}>
-        <Image
-          source={Images.WELCOMEIMAGE}
-          style={styles.imageStyle}
-          resizeMode="contain"
-        />
+      <View style={[commonStyles.parentView, styles.parentView]}>
+        <View
+          style={{
+            alignItems: "center",
+            height: height(50),
+            justifyContent: "center",
+            // backgroundColor: "red",
+          }}
+        >
+          <Image
+            source={Images.WELCOMEIMAGE}
+            style={styles.imageStyle}
+            resizeMode="contain"
+          />
 
-        <Text style={styles.kidzText}>KidzVoice</Text>
+          <Text style={styles.kidzText}>KidzVoice</Text>
 
-        <Text style={styles.growText}>Grow Smarter, Kinder, Happier.</Text>
+          <Text style={styles.growText}>Grow Smarter, Kinder, Happier.</Text>
+        </View>
+        {/* <View style={{ flex: 1, justifyContent: "center" }}> */}
         <Button title="Get Started" />
+        {/* </View> */}
       </View>
     </StatusBarWrapper>
   );
