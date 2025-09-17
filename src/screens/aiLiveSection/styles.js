5a258f4a02315ef9a82ba4d76f811facbea3124f
Ahmad Saffiullah <111109170+ahmadkhan402@users.noreply.github.com>
2025-09-17 19:23:26 +0500

    feeling result screen added

--- src/screens/aiLiveSection/styles.js
+++ src/screens/aiLiveSection/styles.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from "react-native";
 import colors from "../../utils/AppColors";
 import { height } from "../../utils/Dimensions";
+import { fonts } from "../../utils/fonts";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.bloader
+        backgroundColor: colors.bloader,
     },
     img: {
         width: 250,
@@ -13,6 +14,50 @@ export const styles = StyleSheet.create({
         resizeMode: 'contain',
         alignSelf: 'center',
         marginTop: height(4),
+    },
+    textBox: {
+        marginVertical: 16,
+        paddingHorizontal: 20,
+    },
+    questionText: {
+        fontSize: 22,
+        color: '#282727',
+        textAlign: 'center',
+        lineHeight: 33,
+        fontFamily: fonts.bold
+    },
+    optionsContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        marginTop: 20,
+    },
+    optionBox: {
+        width: '44%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 15,
+        marginBottom: 15,
+        backgroundColor: '#f5f5f5',
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 3,
+        flexDirection: 'row',
+        gap: 10,
+        marginHorizontal: 12
 
-    }
+    },
+    optionEmoji: {
+        fontSize: 24,
+    },
+    optionText: {
+        fontSize: 14,
+        marginTop: 5,
+        fontFamily: fonts.regular,
+        color: '#444',
+    },
 })
